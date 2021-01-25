3e1815afb56f1845216b2e86f3688535a8866563
Jan Clarin <me@janclarin.com>
2021-01-25 01:53:39 -0800

    Apply solhint fixes

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.5.28 <0.8.0;
 
 contract Migrations {
     address public owner;
-    uint256 public last_completed_migration;
+    uint256 public lastCompletedMigration;
 
     modifier restricted() {
         if (msg.sender == owner) _;
@@ -14,6 +14,6 @@ contract Migrations {
     }
 
     function setCompleted(uint256 completed) public restricted {
-        last_completed_migration = completed;
+        lastCompletedMigration = completed;
     }
 }
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.5.28 <0.8.0;
 
 contract SimpleStorage {
-    uint256 storedData;
+    uint256 private storedData;
 
     function set(uint256 x) public {
         storedData = x;
