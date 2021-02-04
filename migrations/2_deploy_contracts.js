b211b60e333b01bea7af30197bc9548a4e4b9b08
Chris Hailey <chris.hailey8@gmail.com>
2021-02-03 17:20:19 -0800

    Remove simplestorage

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,3 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var FanProxy = artifacts.require("./FanProxy.sol");
 const FanDonation = artifacts.require("./FanDonation.sol");
 
@@ -8,7 +7,6 @@ const LendingPoolAddressesProvider =
   "0x88757f2f99175387ab4c6a4b3067c77a695b0349";
 
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(FanProxy);
   deployer.deploy(FanDonation, LendingPoolAddressesProvider);
 };
