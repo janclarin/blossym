8f06820dc89531bc5e2ae17159a505ad0349d4d3
Jan Clarin <me@janclarin.com>
2021-02-04 01:13:42 -0800

    Integrate FanDonation into FanProxy

--- contracts/FanProxy.sol
+++ contracts/FanProxy.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./FanDonation.sol";
 import "./FanSwap.sol";
 
 contract FanProxy {
@@ -9,12 +10,14 @@ contract FanProxy {
     address private constant USDC_KOVAN_ADDRESS =
         0xe22da380ee6B445bb8273C81944ADEB6E8450422;
 
-    IERC20 private usdc = IERC20(USDC_KOVAN_ADDRESS);
+    ERC20 private usdc = ERC20(USDC_KOVAN_ADDRESS);
 
     FanSwap private immutable fanSwap;
+    FanDonation private immutable fanDonation;
 
-    constructor(address fanSwapAddress) public {
+    constructor(address fanSwapAddress, address fanDonationAddress) public {
         fanSwap = FanSwap(fanSwapAddress);
+        fanDonation = FanDonation(fanDonationAddress);
     }
 
     function swapAndDonateETH(address to) external payable {
@@ -30,10 +33,7 @@ contract FanProxy {
 
     function _donateUSDC(uint256 amount, address to) private {
         require(amount > 0, "Cannot donate 0 USDC");
-        // TODO: Remove require statement. Added to satisfy "unused var" linter.
-        require(to != address(0), "Required just to use var");
-
-        // TODO: Approve FanDonation contract to spend usdcAmount.
-        // TODO: Call FanDonation.donate(usdcAmount, to);
+        usdc.increaseAllowance(address(fanDonation), amount);
+        fanDonation.donate(amount, to);
     }
 }
