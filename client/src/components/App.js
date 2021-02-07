f6f823e53bae3e28a4acecfe326a614603dea9c1
Jan Clarin <me@janclarin.com>
2021-02-07 01:19:01 -0800

    Bind disconnectWallet function
    Could not reference "this"

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -35,6 +35,7 @@ class App extends Component {
       providerOptions, // required
     });
     this.openWalletConnectModal = this.openWalletConnectModal.bind(this);
+    this.disconnectWallet = this.disconnectWallet.bind(this);
   }
 
   async openWalletConnectModal() {
